eef88af53eec392dab0aa5603d64a9c49241b9d6
Bobby King <bobby@baltivirtual.com>
2020-12-14 02:50:36 -0500

    Updated with cases

--- express/js/mysqlConnectStates.py
+++ express/js/mysqlConnectStates.py
@@ -2,8 +2,8 @@ import mysql.connector
 import geojson
 import ast
 from datetime import datetime, timedelta
-from collections import defaultdict
 
+#change back to one
 yesterday = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
 
 
@@ -27,11 +27,15 @@ myresult = mycursor.fetchall()
 
 i = 0
 
+d = dict()
 
 for x in myresult:
-    d[myresult[i][2]]=myresult[i][4]
+    d[myresult[i][2]] = []
+    d[myresult[i][2]].append(myresult[i][4])
+    d[myresult[i][2]].append(myresult[i][3])
     i+=1
 
+#print(d['01'][0])
 
 
 #states data
@@ -40,7 +44,8 @@ with open(file_states, 'r') as f:
 
 for feature in data['features']:
     if feature['id'] in d:
-        feature['properties']['deaths']=d[feature['id']]
+        feature['properties']['deaths']=d[feature['id']][0]
+        feature['properties']['cases']= d[feature['id']][1]
 
 with open(file_new_states, 'w+') as f:
     geojson.dump(data, f, indent=2)
